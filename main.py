2e9f00f52d10e8d0809c87eeb26b8b7d59902fc7
Andrew Turley <ahturl21@colby.edu>
2020-01-14 10:58:27 -0500

    it draws a SQUARE

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+
     pygame.display.flip()
