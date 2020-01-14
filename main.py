cf484992f71cb4a5d9f8aeb02a5c912f41cdc113
Andrew Turley <ahturl21@colby.edu>
2020-01-14 11:03:40 -0500

    the square MOVES

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 done = False
+x_pos = 30
+y_pos = 30
 
 while not done:
+    screen.fill((0, 0, 0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y_pos -= 3
+    if pressed[pygame.K_DOWN]: y_pos += 3
+    if pressed[pygame.K_LEFT]: x_pos -= 3
+    if pressed[pygame.K_RIGHT]: x_pos += 3
+
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x_pos, y_pos, 60, 60))
 
     pygame.display.flip()
