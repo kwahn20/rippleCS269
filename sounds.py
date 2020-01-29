1f25a408000f192ded418c66eb65085bf9ed5177
Max-Abramson <mabramso11@gmail.com>
2020-01-29 15:17:23 -0500

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -3,8 +3,5 @@ pygame.mixer.init()
 
 button_press_2 = pygame.mixer.Sound('Sounds/home_screen_button_press_2.wav')
 
-# step_1_set_1 = pygame.mixer.Sound('Sounds/step_1.wav')
-# step_2_set_1 = pygame.mixer.Sound('Sounds/step_2.wav')
-
-# step_1_set_2 = pygame.mixer.Sound('Sounds/step_1_set_2.wav')
-# step_2_set_2 = pygame.mixer.Sound('Sounds/step_2_set_2.wav')
+step_1_set_1 = pygame.mixer.Sound('step 1.wav')
+step_2_set_1 = pygame.mixer.Sound('step 2.wav')
