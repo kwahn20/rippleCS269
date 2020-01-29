afdcf71c5da97fe19be241c0c6657be916660791
Michael Coyne <coynem@mssm.org>
2020-01-29 13:33:43 -0500

    added gameover_wait

--- M.py
+++ M.py
@@ -7,6 +7,7 @@ from pygame.locals import *
 START_SCREEN = 'images/titlescreen.png'
 HIGHSCORES = 'images/HighScores.png'
 CREDITS = 'images/Credits.png'
+GAMEOVER = 'images/GameOver.png'
 
 # a class for background images such as the start screen, or any other images
 # that we want in the background (like a background for the game or whatnot)
@@ -74,11 +75,22 @@ def credits_wait():
             show_image(screen, startscreen_background)
             return()
 
+def gameover_wait():
+    back_clicked = make_button_click_trigger(x_range = (19, 128), y_range = (21, 83))
+    show_image(screen, gameover_background)
+    while True:
+        events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        if keys[K_ESCAPE]: sys.exit()
+        if back_clicked(events):
+            startscreen_wait()
+
 screen = pygame.display.set_mode(size = (1000, 563))
 
 startscreen_background = Background(START_SCREEN)
 highscores_image = Background(HIGHSCORES)
 credits_background = Background(CREDITS)
+gameover_background = Background(GAMEOVER)
 
 if(__name__ == '__main__'):
     pass
